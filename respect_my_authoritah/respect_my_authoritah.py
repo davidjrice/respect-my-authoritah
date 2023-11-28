80f343cbfcb753b067ce74943ac4b4d730c1e07a
David Rice <david.rice@slicelife.com>
2023-11-28 23:02:54 +0000

    build: re-enable rest of the script with better logging

--- respect_my_authoritah/respect_my_authoritah.py
+++ respect_my_authoritah/respect_my_authoritah.py
@@ -35,27 +35,27 @@ class Authoritah:
             pyproject = toml.load(file)
 
         # Update authors list
-        pyproject['tool']['poetry']['authors'] = contributors
+        pyproject['project']['authors'] = contributors
 
         # Write back to pyproject.toml
-        # with open('pyproject.toml', 'w') as file:
-        #     toml.dump(pyproject, file)
+        with open('pyproject.toml', 'w') as file:
+            toml.dump(pyproject, file)
 
         # Create a new branch
-        # branch_name = f"update-authors-{uuid.uuid4().hex}"
-        # os.system(f'git checkout -b {branch_name}')
+        branch_name = f"update-authors-{uuid.uuid4().hex}"
+        os.system(f'git checkout -b {branch_name}')
 
         # Commit and push changes
-        # os.system('git config --global user.email "github-actions[bot]@users.noreply.github.com"')
-        # os.system('git config --global user.name "GitHub Actions"')
-        # os.system('git add pyproject.toml')
-        # os.system('git commit -m "Update authors list"')
-        # os.system(f'git push origin {branch_name}')
+        os.system('git config --global user.email "github-actions[bot]@users.noreply.github.com"')
+        os.system('git config --global user.name "GitHub Actions"')
+        os.system('git add pyproject.toml')
+        os.system('git commit -m "Update authors list"')
+        os.system(f'git push origin {branch_name}')
 
         # Create a new pull request
-        # pr_data = {
-        #     'title': 'Update authors list',
-        #     'head': branch_name,
-        #     'base': 'main',
-        # }
-        # response = requests.post(f'https://api.github.com/repos/{repo}/pulls', headers=headers, json=pr_data)
+        pr_data = {
+            'title': 'Update authors list',
+            'head': branch_name,
+            'base': 'main',
+        }
+        response = requests.post(f'https://api.github.com/repos/{repo}/pulls', headers=headers, json=pr_data)
