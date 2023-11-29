0e390dedd0a7b662e083bbf05136a4f5c7c6b02e
David Rice <david.rice@slicelife.com>
2023-11-29 01:02:15 +0000

    fix: use tomlkit comment

--- respect_my_authoritah/respect_my_authoritah.py
+++ respect_my_authoritah/respect_my_authoritah.py
@@ -74,7 +74,7 @@ class Authoritah:
 
         if not authors_field_exists:
             # Add newline after authors field
-            pyproject["project"].add(tomlkit.nl())
+            pyproject["project"].add(tomlkit.comment(""))
 
         # Write back to pyproject.toml
         with open("pyproject.toml", "w") as file:
